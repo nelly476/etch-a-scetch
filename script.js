fb6fe0541bb19b7a5eca28dc8ad0023406418c34
nelly476 <nellybulatova12@gmail.com>
2022-08-21 14:51:38 +0300

    add js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.className = "square";
-  document.getElementById("container").appendChild(square);
-  square.addEventListener("mouseover", function (e) {
-    square.style.cssText = "background: black";
-  });
+for (let r = 0; r < 16; r++) {
+  let x = document.getElementById("table").insertRow(r);
+  for (let c = 0; c < 16; c++) {
+    let y = x.insertCell(c);
+  }
 }
+
+function createTable() {
+  rn = window.prompt("Input number of rows", 1);
+  cn = window.prompt("Input number of columns");
+  table.remove();
+  for (let r = 0; r < parseInt(rn, 10); r++) {
+    let x = document.getElementById("newTable").insertRow(r);
+    for (let c = 0; c < parseInt(cn, 10); c++) {
+      let y = x.insertCell(c);
+    }
+  }
+  document.querySelectorAll("#newTable td").forEach((e) =>
+    e.addEventListener("mouseover", function (e) {
+      this.style.cssText = "background: black";
+    })
+  );
+}
+
+document.querySelectorAll("#table td").forEach((e) =>
+  e.addEventListener("mouseover", function (e) {
+    this.style.cssText = "background: black";
+  })
+);
